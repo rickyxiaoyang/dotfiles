db25fb4f499938b21ada00de5d614376a4e15fc4
Ricky Yang <rickyxiaoyang@gmail.com>
2024-11-03 14:12:10 -0500

    feat: plugins to install

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ else
 	brew doctor
 	OSX=true
 fi
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 if [ $UBUNTU ]; then
 	sudo apt-get -y install git
 fi
@@ -19,3 +19,9 @@ if [ $OSX ]; then
 	brew install git
 fi
 
+# install plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+
+source ~/.zshrc
